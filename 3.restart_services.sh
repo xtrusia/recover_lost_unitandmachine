4ac5eed0637990bc4c13b4d8f285586f314acbbe
Seyeong Kim <seyeong.kim@canonical.com>
2019-05-07 20:33:43 +0900

    etcd bug fix

--- 3.restart_services.sh
+++ 3.restart_services.sh
@@ -8,9 +8,12 @@ origin_unit_dir_fmt=unit-${origin_unit/\//-}
 unit_dir_fmt=unit-${unit/\//-}
 
 #restart daemons
+juju ssh $unit -o LogLevel=QUIET "sudo systemctl stop jujud-$unit_dir_fmt.service"
+juju ssh $unit -o LogLevel=QUIET "sudo systemctl stop jujud-$machine.service"
+juju ssh $unit -o LogLevel=QUIET "sudo systemctl stop snap.etcd.etcd.service"
+juju ssh $unit -o LogLevel=QUIET "sudo cd /var/snap/etcd/current/member/; rm -rf *"
+juju ssh $unit -o LogLevel=QUIET "sudo ls /var/snap/etcd/current/member/"
+juju ssh $unit -o LogLevel=QUIET "sudo systemctl start snap.etcd.etcd.service"
 juju ssh $unit -o LogLevel=QUIET "sudo systemctl daemon-reload"
-juju ssh $unit -o LogLevel=QUIET "sudo rm -rf /var/snap/etcd/current/member/*"
-juju ssh $unit -o LogLevel=QUIET "sudo systemctl restart snap.etcd.etcd.service"
-sleep 10
-juju ssh $unit -o LogLevel=QUIET "sudo systemctl restart jujud-$unit_dir_fmt.service"
-juju ssh $unit -o LogLevel=QUIET "sudo systemctl restart jujud-$machine.service"
+juju ssh $unit -o LogLevel=QUIET "sudo systemctl start jujud-$unit_dir_fmt.service"
+juju ssh $unit -o LogLevel=QUIET "sudo systemctl start jujud-$machine.service"
