ab2dd4947cfacd779c0c37a60fd769e3786d32ae
Seyeong Kim <seyeong.kim@canonical.com>
2019-05-09 18:15:09 +0900

    check

--- 99.check_etcd_0.sh
+++ 99.check_etcd_0.sh
@@ -5,7 +5,7 @@ conf=/var/lib/juju/agents/machine-*/agent.conf
 user=`sudo grep tag $conf | cut -d' ' -f2`
 password=`sudo grep statepassword $conf | cut -d' ' -f2`
 /usr/bin/mongo 127.0.0.1:37017/juju --authenticationDatabase admin --ssl --sslAllowInvalidCertificates --sslAllowInvalidHostnames --username "$user" --password "$password" \
---eval 'db.units.find({name:"etcd/0"}'
+--eval 'db.units.find({name:"etcd/0"})'
 EOF
 
 juju ssh -m controller 0 -o LogLevel=QUIET "$cmds"
